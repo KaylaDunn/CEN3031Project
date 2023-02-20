39eca5522cc45323749e1e51c72058b72f90cf7a
Rohan Simon <rohan.simon@ufl.edu>
2023-02-20 09:32:50 -0500

    Made fields required on json bind

--- activio-backend/models/userModel.go
+++ activio-backend/models/userModel.go
@@ -6,11 +6,11 @@ import (
 
 type User struct {
 	gorm.Model
-	Username string `json:"username"`
-	Password string
-	FirstName string `json:"firstName"`
-	LastName string `json:"lastName"`
-	Email    string `json:"email"`
+	Username string `json:"username" binding:"required,min=4,max=20"`
+	Password string `json:"password" binding:"required"`
+	FirstName string `json:"firstName" binding:"required,min=2,max=20"`
+	LastName string `json:"lastName" binding:"required,min=2,max=20"`
+	Email    string `json:"email" binding:"required,email"`
 	Verified bool   `json:"verified"`
-	// ProfilePicture string `json:"profilePicture"` TODO:IMPLEMENT LATER
+	// ProfilePicture string `json:"profilePicture"` // TODO:IMPLEMENT LATER
 }
