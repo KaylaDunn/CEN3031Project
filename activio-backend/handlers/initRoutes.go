e248aec117d2e947ba7c5e7d26676c7838e48a39
Rohan Simon <rohan.simon@ufl.edu>
2023-03-01 15:27:01 -0500

    Revert later

--- activio-backend/handlers/initRoutes.go
+++ activio-backend/handlers/initRoutes.go
@@ -32,6 +32,21 @@ func InitRoutes(r *gin.Engine) {
 
   // user endpoints
   api.POST("/signup", SignUp)
+  api.OPTIONS("/signup", func(c *gin.Context) {
+    c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+    c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+    c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+    c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, client-security-token")
+    c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+    c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+    c.Writer.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+    if c.Request.Method == "OPTIONS" {
+      c.AbortWithStatus(204)
+      return
+    }
+    
+  })
   api.POST("/login", Login)
   auth.GET("/refreshtoken", RefreshJWT)
 }
