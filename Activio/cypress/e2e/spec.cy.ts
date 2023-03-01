02bf6a592190464299b0d5a2f3f959e4958f5746
Lorenz Carvajal <lorenzcarvajal@ufl.edu>
2023-03-01 01:57:39 -0500

    End-to-end test checking navigation on Login and Create Account buttons

--- Activio/cypress/e2e/spec.cy.ts
+++ Activio/cypress/e2e/spec.cy.ts
@@ -1,6 +1,20 @@
 describe('My First Test', () => {
+
   it('Visits the initial project page', () => {
     cy.visit('/')
-    cy.contains('app is running!')
+    cy.contains('Welcome to Activio!')
   })
+
+  it('navigates to Login page', () => {
+    cy.visit('/')
+    cy.get('button[type="loginBtn"]').click()
+    cy.url().should('eq', 'http://localhost:4200/login')
+  })
+
+  it('navigates to Signup page', () => {
+    cy.visit('/login')
+    cy.get('button[type="signup"]').click()
+    cy.url().should('eq', 'http://localhost:4200/createaccount')
+  })
+
 })
