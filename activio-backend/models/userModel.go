43b3704bfff2d9e18eea8f03ca1ffca8457c44aa
Rohan Simon <rohan.simon@ufl.edu>
2023-03-17 15:18:41 -0400

    restructured user model

--- activio-backend/models/userModel.go
+++ activio-backend/models/userModel.go
@@ -6,11 +6,11 @@ import (
 
 type User struct {
 	gorm.Model
-	Username string `json:"username"`
+	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required"`
+	Username string `json:"username"`
 	FirstName string `json:"firstName"`
 	LastName string `json:"lastName"`
-	Email    string `json:"email" binding:"required,email"`
 	Verified bool   `json:"verified"`
 	ProfilePicture string `json:"profilePicture"`
 }
