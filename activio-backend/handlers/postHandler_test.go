44df71f63b51d00a2747adb3e238c6fdab436094
Todd Cooney <63822499+tmcooney@users.noreply.github.com>
2023-03-21 20:31:29 -0400

    added get posts test

--- activio-backend/handlers/postHandler_test.go
+++ activio-backend/handlers/postHandler_test.go
@@ -86,3 +86,38 @@ func TestCreatePost(t *testing.T) {
 	}
 
 }
+
+func TestGetPosts(t *testing.T) {
+
+	data := map[string]interface{}{
+		"password": "bobspassword",
+		"email":    "bob@ufl.edu",
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Printf("could not marshal json: %s\n", err)
+		return
+	}
+
+	httpposturl := "http://0.0.0.0:3000/api/posts"
+
+	request, _ := http.NewRequest("GET", httpposturl, bytes.NewBuffer(jsonData))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	got := response.Status == "200 OK"
+	want := true
+
+	println(response.Status)
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+}
