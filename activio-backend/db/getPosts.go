28189ccd2a55170820948033b12f2183ec60425c
Rohan Simon <rohan.simon@ufl.edu>
2023-03-27 09:13:08 -0400

    now gets 10 most recent posts

--- activio-backend/db/getPosts.go
+++ activio-backend/db/getPosts.go
@@ -10,7 +10,7 @@ import (
 func GetPosts(limit int, offset int) ([]models.Post, error) {
 	var posts []models.Post
 	db := GetDB()
-	err := db.Limit(limit).Offset(offset).Find(&posts).Error
+	err := db.Statement.Order("created_at DESC").Limit(limit).Offset(offset).Find(&posts).Error
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
