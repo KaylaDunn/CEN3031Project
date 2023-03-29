a7b4d952eb03a257c2f47651711da53b371bf525
Todd Cooney <63822499+tmcooney@users.noreply.github.com>
2023-03-28 23:15:53 -0400

    added TestDeleteUserAndUserDataFail unit test

--- activio-backend/handlers/userHandler_test.go
+++ activio-backend/handlers/userHandler_test.go
@@ -176,6 +176,54 @@ func TestDeleteUserAndUserData(t *testing.T) {
 	}
 }
 
+func TestDeleteUserAndUserDataFail(t *testing.T) {
+
+	//login first
+
+	data := map[string]interface{}{
+		"password": "notbigchunguspassword",
+		"email":    "notbigchungus@ufl.edu",
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Printf("could not marshal json: %s\n", err)
+		return
+	}
+
+	httpposturl := "http://0.0.0.0:3000/api/auth/deleteaccount"
+
+	request, _ := http.NewRequest("DELETE", httpposturl, bytes.NewBuffer(jsonData))
+
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	got := response.Status == "401 Unauthorized"
+	want := true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+
+	body, _ := io.ReadAll(response.Body)
+
+	println("status: " + response.Status)
+	println("body: " + string(body))
+
+	got = string(body) == "{\"error\":\"Unauthorized\"}"
+	want = true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+}
+
 func TestGetUserDetails(t *testing.T) {
 
 	//login first
