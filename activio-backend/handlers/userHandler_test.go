d78a158daa94744aad05bd2236f2e6af467b81ba
Todd Cooney <63822499+tmcooney@users.noreply.github.com>
2023-03-28 22:54:29 -0400

    added refreshJWTFail unit test

--- activio-backend/handlers/userHandler_test.go
+++ activio-backend/handlers/userHandler_test.go
@@ -292,3 +292,73 @@ func TestRefreshJWT(t *testing.T) {
 	}
 
 }
+
+func TestRefreshJWTFail(t *testing.T) {
+
+	//login first
+
+	data := map[string]interface{}{
+		"password": "bobspassword",
+		"email":    "bob@ufl.edu",
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Printf("could not marshal json: %s\n", err)
+		return
+	}
+
+	httpposturl := "http://0.0.0.0:3000/api/login"
+
+	request, _ := http.NewRequest("POST", httpposturl, bytes.NewBuffer(jsonData))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	token := response.Header.Get("Set-Cookie")
+	token = token[14:strings.IndexByte(token, ';')]
+
+	httpposturl = "http://0.0.0.0:3000/api/auth/refreshtoken"
+
+	request, _ = http.NewRequest("GET", httpposturl, bytes.NewBuffer(jsonData))
+
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	//cookie := &http.Cookie{
+	//Name:   "Authorization",
+	//Value:  token,
+	//MaxAge: 86400,
+	//}
+	//request.AddCookie(cookie)
+
+	client = &http.Client{}
+	response, error = client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	got := response.Status == "401 Unauthorized"
+	want := true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+
+	body, _ := io.ReadAll(response.Body)
+
+	println("body: " + string(body))
+
+	got = string(body) == "{\"error\":\"Unauthorized\"}"
+	want = true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+
+}
