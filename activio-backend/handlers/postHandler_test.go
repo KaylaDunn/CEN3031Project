00f23b2a5bf1e05eade2eaf43f978e1e17146434
Todd Cooney <63822499+tmcooney@users.noreply.github.com>
2023-03-28 23:03:55 -0400

    Added TestCreatePostFail unit test

--- activio-backend/handlers/postHandler_test.go
+++ activio-backend/handlers/postHandler_test.go
@@ -76,10 +76,51 @@ func TestCreatePost(t *testing.T) {
 	got := response.Status == "200 OK"
 	want := true
 
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+
+}
+
+func TestCreatePostFail(t *testing.T) {
+
+	data := map[string]interface{}{
+		"postDescription": "Created using api",
+		"longitude":       -5.234,
+		"latitude":        0.234,
+		"locationName":    "paris",
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Printf("could not marshal json: %s\n", err)
+		return
+	}
+
+	httpposturl := "http://0.0.0.0:3000/api/auth/createpost"
+
+	request, _ := http.NewRequest("POST", httpposturl, bytes.NewBuffer(jsonData))
+
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	got := response.Status == "401 Unauthorized"
+	want := true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+
 	body, _ := io.ReadAll(response.Body)
 
-	println("status: " + response.Status)
-	println("body: " + string(body))
+	got = string(body) == "{\"error\":\"Unauthorized\"}"
+	want = true
 
 	if got != want {
 		t.Errorf("got %t, wanted %t", got, want)
