bf1a99d31abced072e3c3147d8141981cef74e9c
Kayla Dunnuck <kayladunnuck@gmail.com>
2023-03-29 17:34:26 -0400

    Creating mock page for login

--- Activio/src/app/logsuccess/logsuccess.component.ts
+++ Activio/src/app/logsuccess/logsuccess.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-logsuccess',
   templateUrl: './logsuccess.component.html',
-  styleUrls: ['./logsuccess.component.css']
+  styleUrls: ['./logsuccess.component.css'],
 })
 export class LogsuccessComponent {
 
