a324a2d1d8f128266a028d08dfceba4da99b564d
Todd Cooney <63822499+tmcooney@users.noreply.github.com>
2023-04-16 16:44:00 -0400

    added TestDeletePost unit test

--- activio-backend/handlers/postHandler_test.go
+++ activio-backend/handlers/postHandler_test.go
@@ -10,6 +10,8 @@ import (
 	"testing"
 )
 
+var postID = ""
+
 func TestCreatePost(t *testing.T) {
 
 	//login first
@@ -72,6 +74,14 @@ func TestCreatePost(t *testing.T) {
 		panic(error)
 	}
 	defer response.Body.Close()
+
+	body, _ := io.ReadAll(response.Body)
+	println("status: " + response.Status)
+	println("body: " + string(body))
+
+	id := string(body)
+	postID = id[14:16]
+	//println("id: " + id[14:16])
 
 	got := response.Status == "200 OK"
 	want := true
@@ -239,13 +249,88 @@ func TestGetPostFail(t *testing.T) {
 
 	body, _ := io.ReadAll(response.Body)
 
-	println("status: " + response.Status)
-	println("body: " + string(body))
-
 	got = string(body) == "{\"message\":\"Post not found\"}"
 	want = true
 
 	if got != want {
 		t.Errorf("got %t, wanted %t", got, want)
 	}
+}
+
+func TestDeletePost(t *testing.T) {
+
+	//login first
+
+	data := map[string]interface{}{
+		"password": "bobspassword",
+		"email":    "bob@ufl.edu",
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Printf("could not marshal json: %s\n", err)
+		return
+	}
+
+	httpposturl := "http://0.0.0.0:3000/api/login"
+
+	request, _ := http.NewRequest("POST", httpposturl, bytes.NewBuffer(jsonData))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	token := response.Header.Get("Set-Cookie")
+	token = token[14:strings.IndexByte(token, ';')]
+
+	data = map[string]interface{}{}
+
+	jsonData, err = json.Marshal(data)
+	if err != nil {
+		fmt.Printf("could not marshal json: %s\n", err)
+		return
+	}
+
+	httpposturl = "http://0.0.0.0:3000/api/auth/deletepost/" + postID
+
+	request, _ = http.NewRequest("DELETE", httpposturl, bytes.NewBuffer(jsonData))
+
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	cookie := &http.Cookie{
+		Name:   "Authorization",
+		Value:  token,
+		MaxAge: 86400,
+	}
+	request.AddCookie(cookie)
+
+	client = &http.Client{}
+	response, error = client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	body, _ := io.ReadAll(response.Body)
+	println("status: " + response.Status)
+	println("body: " + string(body))
+
+	got := response.Status == "200 OK"
+	want := true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+
+	got = string(body) == "{\"message\":\"Post deleted successfully\"}"
+	want = true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+
 }
