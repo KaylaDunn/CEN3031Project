738368f27299f64fed2de13d30a1230384da922c
Todd Cooney <63822499+tmcooney@users.noreply.github.com>
2023-04-16 16:02:06 -0400

    added get post test

--- activio-backend/handlers/postHandler_test.go
+++ activio-backend/handlers/postHandler_test.go
@@ -160,3 +160,47 @@ func TestGetPosts(t *testing.T) {
 		t.Errorf("got %t, wanted %t", got, want)
 	}
 }
+
+func TestGetPost(t *testing.T) {
+
+	data := map[string]interface{}{
+		"password": "bobspassword",
+		"email":    "bob@ufl.edu",
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Printf("could not marshal json: %s\n", err)
+		return
+	}
+
+	httpposturl := "http://0.0.0.0:3000/api/post/1"
+
+	request, _ := http.NewRequest("GET", httpposturl, bytes.NewBuffer(jsonData))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	got := response.Status == "200 OK"
+	want := true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+
+	body, _ := io.ReadAll(response.Body)
+
+	sillyString := "{\"post\":{\"id\":1,\"createdAt\":\"2023-03-20T21:20:52.848Z\",\"postedBy\":2,\"postDescription\":\"Created using api\",\"longitude\":-5.234,\"latitude\":0.234,\"locationName\":\"paris\",\"images\":[],\"comments\":[]}}"
+
+	got = string(body) == sillyString
+	want = true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+}
