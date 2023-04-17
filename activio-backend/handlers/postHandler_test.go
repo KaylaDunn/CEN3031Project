e2b46f26c8fdb28ac1c30e870edc0e46aa0965e8
Todd Cooney <63822499+tmcooney@users.noreply.github.com>
2023-04-17 18:15:49 -0400

    added TestLikePost unit test

--- activio-backend/handlers/postHandler_test.go
+++ activio-backend/handlers/postHandler_test.go
@@ -81,7 +81,6 @@ func TestCreatePost(t *testing.T) {
 
 	id := string(body)
 	postID = id[14:16]
-	//println("id: " + id[14:16])
 
 	got := response.Status == "200 OK"
 	want := true
@@ -205,10 +204,11 @@ func TestGetPost(t *testing.T) {
 
 	body, _ := io.ReadAll(response.Body)
 
-	sillyString := "{\"post\":{\"id\":1,\"createdAt\":\"2023-03-20T21:20:52.848Z\",\"postedBy\":2,\"postDescription\":\"Created using api\",\"longitude\":-5.234,\"latitude\":0.234,\"locationName\":\"paris\",\"images\":[],\"comments\":[],\"numberOfLikes\":0}}"
-
 	println("status: " + response.Status)
 	println("body: " + string(body))
+
+	sillyString := "{\"post\":{\"id\":1,\"createdAt\":\"2023-03-20T21:20:52.848Z\",\"postedBy\":2,\"postDescription\":\"Created using api\",\"longitude\":-5.234,\"latitude\":0.234,\"locationName\":\"paris\",\"images\":[],\"comments\":[],\"numberOfLikes\":1}}"
+
 	got = string(body) == sillyString
 	want = true
 
@@ -257,6 +257,89 @@ func TestGetPostFail(t *testing.T) {
 	if got != want {
 		t.Errorf("got %t, wanted %t", got, want)
 	}
+}
+
+func TestLikePost(t *testing.T) {
+
+	//login first
+
+	data := map[string]interface{}{
+		"password": "bobspassword",
+		"email":    "bob@ufl.edu",
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Printf("could not marshal json: %s\n", err)
+		return
+	}
+
+	httpposturl := "http://0.0.0.0:3000/api/login"
+
+	request, _ := http.NewRequest("POST", httpposturl, bytes.NewBuffer(jsonData))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	token := response.Header.Get("Set-Cookie")
+	token = token[14:strings.IndexByte(token, ';')]
+
+	data = map[string]interface{}{
+		"postDescription": "Created using api",
+		"longitude":       -5.234,
+		"latitude":        0.234,
+		"locationName":    "paris",
+	}
+
+	jsonData, err = json.Marshal(data)
+	if err != nil {
+		fmt.Printf("could not marshal json: %s\n", err)
+		return
+	}
+
+	httpposturl = "http://0.0.0.0:3000/api/auth/likepost/" + postID
+
+	request, _ = http.NewRequest("PUT", httpposturl, bytes.NewBuffer(jsonData))
+
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	cookie := &http.Cookie{
+		Name:   "Authorization",
+		Value:  token,
+		MaxAge: 86400,
+	}
+	request.AddCookie(cookie)
+
+	client = &http.Client{}
+	response, error = client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	body, _ := io.ReadAll(response.Body)
+	println("status: " + response.Status)
+	println("body: " + string(body))
+
+	got := response.Status == "200 OK"
+	want := true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+
+	got = string(body) == "{\"message\":\"Post liked successfully\"}"
+	want = true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+
 }
 
 func TestDeletePost(t *testing.T) {
