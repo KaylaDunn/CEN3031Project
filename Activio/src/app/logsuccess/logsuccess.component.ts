18e34878b53f6a8729f1000e4db5397d6a8fd596
Lorenz Carvajal <lorenzcarvajal@ufl.edu>
2023-04-17 17:28:54 -0400

    Posts are visible from front page, missing automatic ability

--- Activio/src/app/logsuccess/logsuccess.component.ts
+++ Activio/src/app/logsuccess/logsuccess.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-logsuccess',
   templateUrl: './logsuccess.component.html',
@@ -6,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class LogsuccessComponent {
 
+  posts: any[] = [];
+  
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit(): void {
+
+    // GET request for the posts
+    this.http.get<any>('http://localhost:3000/api/posts')
+
+    .subscribe(response => {
+
+      // (debugging) log entire array
+      console.log(response.posts); 
+
+      this.posts = response.posts;
+
+      // (debugging) iterate through array
+      response.posts.forEach((post: any) => {
+        console.log(post.postDescription);
+      })
+    }, error => {
+      console.error('Error retrieving posts.');
+    })
+
+    // // temp: clear posts for testing
+    // const id = 1;
+    // this.http.delete(`http://localhost:3000/api/auth/deletepost/${id}`, {
+    //   withCredentials: true
+    // })
+    //   .subscribe(response => {
+    //     console.log(response);
+    //   })
+  }
 }
