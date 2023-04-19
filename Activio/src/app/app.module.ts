a518a56565aecccd17ada5e09ff4e5ef84b2dc7d
Kayla Dunnuck <kayladunnuck@gmail.com>
2023-04-19 08:15:54 -0400

    Fixing merge errors

--- Activio/src/app/app.module.ts
+++ Activio/src/app/app.module.ts
@@ -32,11 +32,8 @@ import { PostComponent } from './post/post.component';
 import { LogoutComponent } from './logout/logout.component';
 import { CreateAccountVerifComponent } from './create-account-verif/create-account-verif.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-
-
 import { LocationSearchComponent } from './location-search/location-search.component';
 import { EditProfileInfoComponent } from './edit-profile-info/edit-profile-info.component';
-
 import { MatDialogModule } from '@angular/material/dialog';
 import { CommentDialogComponent } from './comment-dialog/comment-dialog.component';
 
