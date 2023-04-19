3a1f8df67d5172e57955ae99033781d5d7e08130
Todd Cooney <63822499+tmcooney@users.noreply.github.com>
2023-04-19 14:19:11 -0400

    added TestCommentOnPost

--- activio-backend/handlers/postHandler_test.go
+++ activio-backend/handlers/postHandler_test.go
@@ -317,8 +317,6 @@ func TestLikePost(t *testing.T) {
 	defer response.Body.Close()
 
 	body, _ := io.ReadAll(response.Body)
-	println("status: " + response.Status)
-	println("body: " + string(body))
 
 	got := response.Status == "200 OK"
 	want := true
@@ -328,6 +326,85 @@ func TestLikePost(t *testing.T) {
 	}
 
 	got = string(body) == "{\"message\":\"Post liked successfully\"}"
+	want = true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+
+}
+
+func TestCommentOnPost(t *testing.T) {
+
+	//login first
+	// test commenting post that was just created and is about to be deleted
+
+	data := map[string]interface{}{
+		"password": "bobspassword",
+		"email":    "bob@ufl.edu",
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Printf("could not marshal json: %s\n", err)
+		return
+	}
+
+	httpposturl := "http://0.0.0.0:3000/api/login"
+
+	request, _ := http.NewRequest("POST", httpposturl, bytes.NewBuffer(jsonData))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	token := response.Header.Get("Set-Cookie")
+	token = token[14:strings.IndexByte(token, ';')]
+
+	data = map[string]interface{}{
+		"comment": "oh wow, what a great post.",
+	}
+
+	jsonData, err = json.Marshal(data)
+	if err != nil {
+		fmt.Printf("could not marshal json: %s\n", err)
+		return
+	}
+
+	httpposturl = "http://0.0.0.0:3000/api/auth/comment/" + postID
+
+	request, _ = http.NewRequest("PUT", httpposturl, bytes.NewBuffer(jsonData))
+
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	cookie := &http.Cookie{
+		Name:   "Authorization",
+		Value:  token,
+		MaxAge: 86400,
+	}
+	request.AddCookie(cookie)
+
+	client = &http.Client{}
+	response, error = client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	body, _ := io.ReadAll(response.Body)
+
+	got := response.Status == "200 OK"
+	want := true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+
+	got = string(body) == "{\"comment\":\"oh wow, what a great post.\",\"message\":\"Comment created successfully\",\"postID\":"+postID+"}"
 	want = true
 
 	if got != want {
