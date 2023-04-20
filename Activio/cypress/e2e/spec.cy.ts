a7d14b094a9e5b725c1ffeec60bce98034b8f70f
Lorenz Carvajal <lorenzcarvajal@ufl.edu>
2023-04-19 21:27:52 -0400

    End to end testing

--- Activio/cypress/e2e/spec.cy.ts
+++ Activio/cypress/e2e/spec.cy.ts
@@ -1,3 +1,4 @@
+import { CreateaccountComponent } from './../../src/app/createaccount/createaccount.component';
 // reference: https://testing-angular.com/end-to-end-testing/#interacting-with-elements
 
 describe('Activio End-to-End', () => {
@@ -22,6 +23,14 @@ describe('Activio End-to-End', () => {
     cy.url().should('eq', 'http://localhost:4200/logsuccess')
   })
 
+  it('does not log in with invalid credentials', () => {
+    cy.visit('/login')
+    cy.get('input[type="text"]').type('fake@email.com')
+    cy.get('input[type="password"]').type('fakePassword')
+    cy.get('button[data-cy="b1"]').click()
+    cy.url().should('eq', 'http://localhost:4200/login')
+  })
+
   it('navigates to Signup page', () => {
     cy.visit('/login')
     cy.get('button[data-cy="b2"]').click()
@@ -39,5 +48,54 @@ describe('Activio End-to-End', () => {
     cy.get('button[type="homeBtn"]').click()
     cy.url().should('eq', 'http://localhost:4200/logsuccess')
   })
+
+  it('createaccount navigates to verification', () => {
+    cy.visit('/createaccount')
+    cy.get('input[type="firstName"]').type('First')
+    cy.get('input[type="lastName"]').type('Last')
+    cy.get('input[type="email"]').type('this@email.com')
+    cy.get('input[type="user"]').type('myUser')
+    cy.get('input[type="birth"]').type('01/01/2001')
+    cy.get('input[type="phone"]').type('(000)-000-000')
+    cy.get('input[type="password1"]').type('pass1')
+    cy.get('input[type="password2"]').type('pass1')
+    cy.get('button[data-cy="signup"]').click()
+    cy.url().should('eq', 'http://localhost:4200/create-account-verif')
+  })
+
+  it('verification navigates back to login', () => {
+    cy.visit('/create-account-verif')
+    cy.wait(6000)
+    cy.url().should('eq', 'http://localhost:4200/login')
+  })
+
+  it('profile navigates to userProfile', () => {
+    cy.visit('/logsuccess')
+    cy.get('button[type="ProfileBttn"]').click()
+    cy.url().should('eq', 'http://localhost:4200/userProfile')
+  })
+
+  it('edit account goes to edit account from userProfile', () => {
+    cy.visit('/userProfile')
+    cy.get('button[data-cy="editBtn"]').click()
+    cy.url().should('eq', 'http://localhost:4200/editAccount')
+  })
+
+  it('user can log out', () => {
+    cy.visit('/logsuccess')
+    cy.get('button[type="LogBtn"]').click()
+    cy.url().should('eq', 'http://localhost:4200/logout')
+  })
+
+  it('logout redirects to main', () => {
+    cy.visit('/logout')
+    cy.wait(6000)
+    cy.url().should('eq', 'http://localhost:4200/')
+  })
   
+  it('post home button goes home', () => {
+    cy.visit('/post')
+    cy.get('button[type="homeBtn"]').click()
+    cy.url().should('eq', 'http://localhost:4200/logsuccess')
+  })
 })
